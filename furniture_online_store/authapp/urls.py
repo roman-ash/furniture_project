a18594079411de3108a9fcd0bfeaf73e0da354fe
Роман Аширов <roman-ash@MacBook-Pro-Roman.local>
2021-10-26 21:35:55 +0500

    stage4.2

--- furniture_online_store/authapp/urls.py
+++ furniture_online_store/authapp/urls.py
@@ -5,4 +5,6 @@ import authapp.views as authapp
 urlpatterns = [
     path('login/', authapp.login, name='login'),
     path('logout/', authapp.logout, name='logout'),
+    path('register/', authapp.register, name='register'),
+    path('edit/', authapp.edit, name='edit'),
 ]
